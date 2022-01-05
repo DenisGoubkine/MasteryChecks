630d34ca11c38fa89934e4714c3215d0742a1d00
DenisGoubkine <denis.goubkine23@gmail.com>
2022-01-05 13:52:33 -0500

    p2

--- 5.1/list0.py
+++ 5.1/list0.py
@@ -1,7 +1,10 @@
-my_list = [-113]
+my_list = []
 
+for n in range(10):
+    my_list.append(-113)
+    
 for i in range(10):
-    print(f'Slot {i} contains a {my_list}')
+    print(f'Slot {i} contains a {my_list[i]}')
     
     
     
